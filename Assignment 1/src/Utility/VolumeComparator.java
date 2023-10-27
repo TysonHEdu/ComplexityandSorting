dce8e3cd8b87bb072e9671858071de3d03c50af9
TysonHEdu <tyson.harder@edu.sait.ca>
2023-10-27 15:10:37 -0600

    Syntax Change
    Will now print different results depending on check

--- Assignment 1/src/Utility/VolumeComparator.java
+++ Assignment 1/src/Utility/VolumeComparator.java
@@ -9,7 +9,7 @@ public class VolumeComparator implements Comparator<Shape> {
 	public int compare(Shape s1, Shape s2) {
 		if(s1.getVolume()> s2.getVolume())
 			return 1;
-		else if (s1.getVolume()> s2.getVolume())
+		else if (s1.getVolume()< s2.getVolume())
 			return -1;
 		return 0;
 	}
